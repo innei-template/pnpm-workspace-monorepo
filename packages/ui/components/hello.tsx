b59d1660e36487a357d93f9cb12d7943dff7a093
Innei <tukon479@gmail.com>
2022-02-16 10:55:56 +0800

    feat: add dev and watch example

--- packages/ui/components/hello.tsx
+++ packages/ui/components/hello.tsx
@@ -1,9 +1,3 @@
-import { add } from '@test/utils'
 import React, { FC } from 'react'
 
-export const Hello: FC = () => (
-  <>
-    {'Hello!'}
-    {add(1, 2)}
-  </>
-)
+export const Hello: FC = () => <>{'Hello! World'}</>
