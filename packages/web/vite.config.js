28dbc8e43454e7ec3250a19fba625fe17c348772
Innei <tukon479@gmail.com>
2023-02-10 11:42:34 +0800

    fix: web playground dev, closes #3
    Signed-off-by: Innei <tukon479@gmail.com>

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,7 +1,19 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
+const __dirname = resolve(import.meta.url.replace('file://', ''), '..')
 
 export default defineConfig({
   base: '',
   plugins: [tsconfigPaths()],
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@test/ui': resolve(__dirname, '../ui/src'),
+      '@test/utils': resolve(__dirname, '../utils/src'),
+    },
+  },
 })
